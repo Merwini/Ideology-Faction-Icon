9c297256cb70bb242346f4e5f67c799b05680c97
Matthew Erwin <me.megalomatt@gmail.com>
2023-01-26 23:49:29 -0500

    Major changes: Added mod settings Added ability to change non-player factions' settlements in the same way as the player faction Added additional handling of variables to try to eliminate null reference exceptions

--- Ideology Faction Icon/Main.cs
+++ Ideology Faction Icon/Main.cs
@@ -11,10 +11,54 @@ using System.Reflection;
 
 namespace Ideology_Faction_Icon
 {
+    // ROADMAP
+    // TODO: CUSTOMIZABLE SETTINGS
+    // TODO: CHANGE TO ONLY OCCUR WHEN WORLD MAP IS OPENED INSTEAD OF EVERY TICK
+    // TODO: ACCOUNT FOR NULLS
+
     [StaticConstructorOnStartup]
-    public partial class Base
+    public class IdeoFactIcon : Mod
     {
-        public static List<Texture2D> ideoIcon = new List<Texture2D>();
+        IdeoFactIconSettings ifiSettings;
+        public IdeoFactIcon(ModContentPack content) : base(content)
+        {
+            this.ifiSettings = GetSettings<IdeoFactIconSettings>();
+        }
+
+       ///  <param name = "inRect" >
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("Change Player Faction Icon", ref IdeoFactIconSettings.changePlayerIcon, "Changes the player's faction settlement icon on the world map to match their primary ideoligion's icon.");
+            listingStandard.CheckboxLabeled("Change Player Faction Color", ref IdeoFactIconSettings.changePlayerIconColor, "Change the player's faction settlement icon color to match the primary ideoligion's color.");
+            listingStandard.CheckboxLabeled("Change Nonplayer Faction Icons", ref IdeoFactIconSettings.changeNonplayerIcons, "Changes all other factions' settlement icons to match their primary ideoligion's icon.");
+            listingStandard.CheckboxLabeled("Change Nonplayer Faction Colors", ref IdeoFactIconSettings.changeNonplayerColors, "Change all other factions' settlement icons to match their primary ideoligion's color.");
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Ideoligion Icon as Faction Icon";
+        }
+    }
+
+    public class IdeoFactIconSettings : ModSettings
+    {
+        public static bool changePlayerIcon = true;
+        public static bool changePlayerIconColor = true;
+        public static bool changeNonplayerIcons = false;
+        public static bool changeNonplayerColors = false;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref changePlayerIcon, "changePlayerIcon");
+            Scribe_Values.Look(ref changePlayerIconColor, "changePlayerIconColor");
+            Scribe_Values.Look(ref changeNonplayerIcons, "changeNonplayerIcons");
+            Scribe_Values.Look(ref changeNonplayerColors, "changeNonplayerColors");
+            base.ExposeData();
+        }
     }
 
     [StaticConstructorOnStartup]
@@ -23,8 +67,8 @@ namespace Ideology_Faction_Icon
         private static readonly Type patchType = typeof(HarmonyPatches);
         static HarmonyPatches()
         {
-            Harmony harmony = new Harmony(id: "rimWorld.nuff.ideofacticon");
-            harmony.Patch(AccessTools.Method(typeof(WorldInterface), nameof(WorldInterface.WorldInterfaceUpdate)), prefix: new HarmonyMethod(patchType, nameof(WorldInterfaceUpdatePrefix))); 
+            Harmony harmony = new Harmony(id: "rimworld.nuff.ideofacticon");
+            harmony.Patch(AccessTools.Method(typeof(WorldInterface), nameof(WorldInterface.WorldInterfaceUpdate)), prefix: new HarmonyMethod(patchType, nameof(WorldInterfaceUpdatePrefix)));
 
             /*
             HarmonyInstance harmonyInstance = HarmonyInstance.Create("your_instance_name");
@@ -34,41 +78,105 @@ namespace Ideology_Faction_Icon
 
         public static void WorldInterfaceUpdatePrefix()
         {
-            Faction pFaction = Find.FactionManager.FirstFactionOfDef(DefDatabase<FactionDef>.GetNamed("PlayerColony", true));
-            bool flag = pFaction.ideos.PrimaryIdeo != null;
-            if (flag)
+            if (IdeoFactIconSettings.changePlayerIcon)
             {
-                SetFactionIcon(ref pFaction, GetIdeoIcon(ref pFaction));
+                Faction pFaction = Find.FactionManager.FirstFactionOfDef(DefDatabase<FactionDef>.GetNamed("PlayerColony", true));
+                bool flag = pFaction.ideos.PrimaryIdeo != null;
+                if(flag)
+                {
+                    SetFactionIcon(ref pFaction, GetIdeoIcon(ref pFaction));
+                }
+            }
+
+            if (IdeoFactIconSettings.changePlayerIconColor)
+            {
+                //Faction pFaction = Find.FactionManager.FirstFactionOfDef(DefDatabase<FactionDef>.GetNamed("PlayerColony", true));
+                Faction pFaction = Find.FactionManager.OfPlayer;
+                bool flag = pFaction.ideos.PrimaryIdeo != null;
+                if (flag)
+                {
+                    Color pFactionColor = GetColor(ref pFaction);
+                    foreach (RimWorld.Planet.Settlement settlement in Find.WorldObjects.SettlementBases)
+                    {
+                        if (settlement.Faction.IsPlayer)
+                        {
+                            SetSettlementColor(settlement, pFactionColor);
+                        }
+                    }
+                }
+            }
+
+            if (IdeoFactIconSettings.changeNonplayerIcons)
+            {
+                List<Faction> npcFactions = new List<Faction>();
+                foreach (Faction faction in Find.FactionManager.AllFactionsListForReading)
+                {
+                    if (!faction.IsPlayer)
+                    {
+                        npcFactions.Add(faction);
+                    }
+                }
+
+                Faction aFaction;
+                foreach (Faction faction in npcFactions)
+                {
+                    aFaction = faction;
+                    bool flag1 = aFaction.ideos != null;
+                    bool flag2 = false;
+                    if (flag1)
+                    {
+                        flag2 = aFaction.ideos.PrimaryIdeo != null;
+                    }
+                    if (flag2)
+                    {
+                        SetFactionIcon(ref aFaction, GetIdeoIcon(ref aFaction));
+                    }
+                }
+            }
+
+            if (IdeoFactIconSettings.changeNonplayerColors)
+            {
+                Faction aFaction;
+                foreach (RimWorld.Planet.Settlement settlement in Find.WorldObjects.SettlementBases)
+                {
+                    if (!settlement.Faction.IsPlayer)
+                    {
+                        aFaction = settlement.Faction;
+                        bool flag1 = aFaction.ideos != null;
+                        bool flag2 = aFaction.ideos.PrimaryIdeo != null;
+                        if (flag1 && flag2)
+                        {
+                            SetSettlementColor(settlement, GetColor(ref aFaction));
+                        }
+                    }
+                }
             }
         }
 
-        private static Texture2D GetIdeoIcon(ref Faction pFaction)
+        private static Texture2D GetIdeoIcon(ref Faction faction)
         {
-            Ideo mainIdeo = pFaction.ideos.PrimaryIdeo;
+            Ideo mainIdeo = faction.ideos.PrimaryIdeo;
             Texture2D ideoIcon = mainIdeo.iconDef.Icon;
             return ideoIcon;
         }
 
+        private static Color GetColor(ref Faction faction)
+        {
+            Color ideoColor = faction.ideos.PrimaryIdeo.colorDef.color;
+            //Color newColor = new Color(ideoColor.r, ideoColor.g, ideoColor.b);
+            return new Color(ideoColor.r, ideoColor.g, ideoColor.b);
+        }
 
-        private static void SetFactionIcon(ref Faction pFaction, Texture2D newIcon)
+        private static void SetFactionIcon(ref Faction faction, Texture2D newIcon)
         {
             Type reflectionSucks = typeof(FactionDef);
             FieldInfo reflectionBlows = reflectionSucks.GetField("factionIcon", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-            reflectionBlows.SetValue(pFaction.def, newIcon);
+            reflectionBlows.SetValue(faction.def, newIcon);
+        }
 
-            //pFaction.color = pFaction.ideos.PrimaryIdeo.colorDef.color;
-
-            Color ideoColor = pFaction.ideos.PrimaryIdeo.colorDef.color;
-            Color newColor = new Color(ideoColor.r, ideoColor.g, ideoColor.b);
-
-            foreach (RimWorld.Planet.Settlement settlement in Find.WorldObjects.SettlementBases)
-            {
-                if (settlement.Faction.IsPlayer)
-                {
-                    settlement.Material.color = newColor;
-                    //settlement.Material.color = pFaction.ideos.PrimaryIdeo.colorDef.color;
-                }
-            }
+        private static void SetSettlementColor(RimWorld.Planet.Settlement settlement, Color color)
+        {
+            settlement.Material.color = color;
         }
     }
 }
